e1e543f6144b348eb2674056129aa981584e7ee7
Franc0du <franc0edu24@gmail.com>
2023-02-27 14:31:56 -0300

    feat: Finished first step o navbar, started button component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import { AppProps } from "next/app" // defininir tipagem para os componentes
+import { Header } from "../components/Header"
 import "../styles/global.scss"  
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+    
+  )
 }
