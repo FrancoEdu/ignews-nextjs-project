16e35aa42a4ca54c170ed1d4d8102726bc047673
Franc0du <franc0edu24@gmail.com>
2023-02-28 20:32:27 -0300

    feat: created method os sign using oauth github using next-auth

--- src/components/Header/ButtonSignIn/index.tsx
+++ src/components/Header/ButtonSignIn/index.tsx
@@ -1,19 +1,22 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn,signOut,useSession } from 'next-auth/react'
 import styles from './styles.module.scss'
 
 export function ButtonSignIn(){
 
-    const isUserLoggedIn = false
+    const{data: session} = useSession()
 
-    return isUserLoggedIn ? (
+    console.log(session)
+
+    return session ? (
         <button type="button" className={styles.btnSignIn}>
             <FaGithub color='#04d361'/>
-            User.name
-            <FiX color='#737380' className={styles.closeIcon} />
+            {session.user.name}
+            <FiX color='#737380' className={styles.closeIcon} onClick={()=> signOut()}/>
         </button>
     ) : (
-        <button type="button" className={styles.btnSignIn}>
+        <button type="button" className={styles.btnSignIn} onClick={() => signIn('github')}>
             <FaGithub color='#eba417'/>
             Sign in with Github
         </button>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { AppProps } from "next/app" // defininir tipagem para os componentes
 import { Header } from "../components/Header"
+import { SessionProvider } from "next-auth/react"
+
 import "../styles/global.scss"  
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header></Header>
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <Header></Header>
+        <Component {...pageProps} />
+      </SessionProvider>  
     </>
     
   )
