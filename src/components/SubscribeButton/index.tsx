327b97ede9b1ae3a26291e4cec7c7a923ab95354
Franc0du <franc0edu24@gmail.com>
2023-03-01 16:31:37 -0300

    Problem with api

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
+import { Session } from 'inspector'
 import { signIn, useSession } from 'next-auth/react'
+import { api } from '../../services/api'
+import { getStripeJS } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps{
@@ -7,12 +10,24 @@ interface SubscribeButtonProps{
 
 export function SubscribeButton({priceId}:SubscribeButtonProps){
 
-    const{data: session} = useSession()
+    const {data: session} = useSession()
 
-    function handleSubscribe(){
+    async function handleSubscribe(){
         if(!session){
             signIn('github')
             return
+        }
+
+        try{
+            const response = await api.post('/subscribe')
+
+            const {sessionId} = response.data;
+
+            const stripe = await getStripeJS()
+
+            await stripe.redirectToCheckout({sessionId})
+        }catch(err){
+            alert(err.message)
         }
     }
 
